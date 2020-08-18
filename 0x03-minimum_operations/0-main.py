2a140f6a57a50f2a3bedc6ee9bca9813ff70b685
afinesami <samibenomar84@gmail.com>
2020-08-18 22:26:05 +0100

    0-main.py

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
