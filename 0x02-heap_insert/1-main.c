a220a7d2764d1dadf103c5dbcefcac7d789aaf74
lafine sami <samibenomar84@gmail.com>
2021-02-12 20:57:57 +0100

    1 main

--- 0x02-heap_insert/1-main.c
+++ 0x02-heap_insert/1-main.c
@@ -3,6 +3,21 @@
 #include "binary_trees.h"
 
 /**
+ * _binary_tree_delete - Deallocate a binary tree
+ *
+ * @tree: Pointer to the root of the tree to delete
+ */
+static void _binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree)
+    {
+        _binary_tree_delete(tree->left);
+        _binary_tree_delete(tree->right);
+        free(tree);
+    }
+}
+
+/**
  * main - Entry point
  *
  * Return: 0 on success, error code on failure
@@ -37,5 +52,6 @@ int main(void)
     node = heap_insert(&root, 50);
     printf("\nInserted: %d\n", node->n);
     binary_tree_print(root);
+    _binary_tree_delete(root);
     return (0);
 }
