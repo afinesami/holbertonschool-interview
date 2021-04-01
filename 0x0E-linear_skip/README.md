09536e0826c4438a6a2d733739512764ea387217
lafine sami <samibenomar84@gmail.com>
2021-04-01 23:55:16 +0200

    linear skip

---
+++ 0x0E-linear_skip/README.md
@@ -0,0 +1,3 @@
+# 0x0E. Linear search in skip list
+
+### Specializations - Interview Preparation ― Algorithms
---
+++ 0x0E-linear_skip/search.h
@@ -0,0 +1,34 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *head, int value);
+
+#endif /* _SEARCH_H_ */
