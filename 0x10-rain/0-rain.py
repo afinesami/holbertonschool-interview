b795b4c7046febf1cd282dcc122eb559a060997b
lafine sami <samibenomar84@gmail.com>
2021-04-15 22:39:17 +0100

    rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
 0. Rain mandatory
-Given a list of non-negative integers
+
+Given a list of non-negative integers representing walls of width 1,
+calculate how much water will be retained after it rains.
 """
 
 
 def rain(walls):
     """
     0. Rain mandatory
+
+    Given a list of non-negative integers representing walls of width 1,
+    calculate how much water will be retained after it rains.
     """
     if (len(walls) < 2):
         return 0
