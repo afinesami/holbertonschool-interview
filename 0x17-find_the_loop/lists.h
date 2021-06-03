682ebe4870f734ef212e747909dd02b093934e6a
lafine sami <samibenomar84@gmail.com>
2021-06-03 22:12:57 +0100

    lists.h

---
+++ 0x17-find_the_loop/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *find_listint_loop(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+
+#endif /* LISTS_H */
